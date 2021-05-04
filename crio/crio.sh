7d63fc725bf6e3a61f4f5ef54417aa381ec6d648
amitsharma <55508548+amit17133129@users.noreply.github.com>
2021-05-04 14:46:43 +0530

    Create crio.sh

---
+++ crio/crio.sh
@@ -0,0 +1,14 @@
+OS=xUbuntu_20.04
+VERSION=1.20
+
+cat >>/etc/apt/sources.list.d/devel:kubic:libcontainers:stable.list<<EOF
+deb https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/$OS/ /
+EOF
+
+cat >>/etc/apt/sources.list.d/devel:kubic:libcontainers:stable:cri-o:$VERSION.list<<EOF
+deb http://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable:/cri-o:/$VERSION/$OS/ /
+EOF
+
+curl -L https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/$OS/Release.key | apt-key --keyring /etc/apt/trusted.gpg.d/libcontainers.gpg add -
+
+curl -L https://download.opensuse.org/repositories/devel:kubic:libcontainers:stable:cri-o:$VERSION/$OS/Release.key | apt-key --keyring /etc/apt/trusted.gpg.d/libcontainers-cri-o.gpg add -
